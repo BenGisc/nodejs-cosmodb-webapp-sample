665d945a0a0bcd9258fc9796eb966420ab9a651c
BenGisc <benoit.gisclong@gmail.com>
2018-03-01 17:43:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
-config.host = process.env.HOST || "https://bgicosmos01.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "tT6oKQL2NFwaTX9hvKmLuuFkGOCFqRjU0pz166i7g0cJzeW2kx5Y3Aj4XkwI4uWduoi5TiNcpmEkfwhsfEZyww==";
+config.host = process.env.HOST;
+config.authKey = process.env.AUTH_KEY;
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
